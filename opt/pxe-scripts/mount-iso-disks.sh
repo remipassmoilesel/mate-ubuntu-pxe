7297ad9980fa3633d1f0ca629fff27d285f18589
Rémi Passmoilesel <remi.passmoilesel@gmail.com>
2016-12-03 11:37:59 +0000

    Update paths and file tree

---
+++ opt/pxe-scripts/mount-iso-disks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# samedi 3 décembre 2016, 11:31:34 (UTC+0000)
+
+mount -o loop /opt/pxe-iso/ubuntu-mate-16.04.1-desktop-amd64.iso 	/srv/iso/mate-ubuntu/16.04/amd64
+mount -o loop /opt/pxe-iso/ubuntu-mate-16.04.1-desktop-i386.iso		/srv/iso/mate-ubuntu/16.04/i386
+
